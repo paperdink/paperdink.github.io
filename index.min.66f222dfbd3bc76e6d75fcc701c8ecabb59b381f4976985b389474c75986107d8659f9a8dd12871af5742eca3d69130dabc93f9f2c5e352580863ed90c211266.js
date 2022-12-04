5fdba05cedc874982867d406c0c093e41dd77f4f
rgujju <rgujju@users.noreply.github.com>
2022-12-04 19:02:38 +0000

    deploy: 7494c490386edf2efa1a8cacf89b800044b47c15

--- index.min.66f222dfbd3bc76e6d75fcc701c8ecabb59b381f4976985b389474c75986107d8659f9a8dd12871af5742eca3d69130dabc93f9f2c5e352580863ed90c211266.js
+++ index.min.66f222dfbd3bc76e6d75fcc701c8ecabb59b381f4976985b389474c75986107d8659f9a8dd12871af5742eca3d69130dabc93f9f2c5e352580863ed90c211266.js
@@ -17,13 +17,20 @@ var suggestions=document.getElementById("suggestions"),userinput=document.getEle
 \u003c/code\u003e\u003c/pre\u003e
 \u003col start="3"\u003e
 \u003cli\u003eGo to \u003ccode\u003eTools \u0026gt; Board \u0026gt; Boards Manager\u003c/code\u003e \u0026amp; install \u003ccode\u003eESP32 by Espressif Systems\u003c/code\u003e board by searching for the same.\u003c/li\u003e
-\u003cli\u003eYou should be able to see ESP32 Arduino boards in \u003ccode\u003eTools \u0026gt; Board\u003c/code\u003e\u003c/li\u003e
+\u003cli\u003eESP32 Arduino boards should be seen in \u003ccode\u003eTools \u0026gt; Board\u003c/code\u003e.\u003c/li\u003e
 \u003cli\u003epaperd.ink works with the \u003ccode\u003eESP32 Dev Module\u003c/code\u003e board.\u003c/li\u003e
+\u003cli\u003eTo quickly get started, the paperdink library can be installed from the Arduino library manager.
+Just go to Arduino library manager and search for \u003ccode\u003ePaperdink\u003c/code\u003e and install the latest version.
+Arduino IDE should automatically install all dependencies.
+You can now see various examples in \u003ccode\u003eFile \u0026gt; Examples \u0026gt; Paperdink\u003c/code\u003e.
+To learn more about the examples and how to configure them, visit the \u003ca href="https://github.com/paperdink/PaperdInk-Library/tree/main/examples"\u003egithub page\u003c/a\u003e\u003c/li\u003e
 \u003c/ol\u003e
 \u003ch2 id="install-esp32-sketch-data-upload"\u003eInstall ESP32 Sketch Data Upload\u003c/h2\u003e
-\u003cp\u003eCheckout this tutorial from random nerd tutorials \u003ca href="https://randomnerdtutorials.com/install-esp32-filesystem-uploader-arduino-ide/"\u003ehere →\u003c/a\u003e\u003c/p\u003e
+\u003cp\u003eSome examples require uploading bitmaps to the ESP32 flash storage.
+Checkout this tutorial from random nerd tutorials \u003ca href="https://randomnerdtutorials.com/install-esp32-filesystem-uploader-arduino-ide/"\u003ehere →\u003c/a\u003e
+to learn how to install the tool and upload data.\u003c/p\u003e
 \u003ch2 id="install-cp210x-usb-to-uart-bridge-vcp-drivers-windows-only"\u003eInstall CP210x USB to UART Bridge VCP Drivers (Windows only)\u003c/h2\u003e
-\u003cp\u003eIf windows is not able to recognize paperd.ink please install the CP210x driver by downloading the setup from the top menu \u003ca href="https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers"\u003ehere →\u003c/a\u003e\u003c/p\u003e
+\u003cp\u003eIf windows is not able to recognize your paperd.ink device please install the CP210x driver by downloading the setup from the top menu \u003ca href="https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers"\u003ehere →\u003c/a\u003e\u003c/p\u003e
 `},{id:1,href:"https://docs.paperd.ink/docs/software/libraries/",title:"2. Libraries Used",description:"Installing libraries in Arduino IDE",content:`\u003cdiv class="alert alert-warning d-flex" role="alert"\u003e
   \u003cdiv class="flex-shrink-1 alert-icon"\u003e💡\u003c/div\u003e
   \u003cdiv class="w-100"\u003eYou can go to Tools \u003e Manage Libraries in the Arduino IDE to search and install the latest versions of following libraries.\u003c/div\u003e
@@ -68,7 +75,7 @@ var suggestions=document.getElementById("suggestions"),userinput=document.getEle
 \u003cp\u003e\u003cstrong\u003e\u003ca href="https://github.com/paperdink/minimal_micropython"\u003ehttps://github.com/paperdink/minimal_micropython\u003c/a\u003e\u003c/strong\u003e\u003c/p\u003e
 `},{id:3,href:"https://docs.paperd.ink/docs/software/community_contributions/",title:"4. Community Contributions",description:"Projects from the community",content:`\u003cp\u003eWe are excited to see what you build with paperd.ink. Send an email to \u003ccode\u003eprasad@paperd.ink\u003c/code\u003e with a link to your project and we will list it here.\u003c/p\u003e
 `},{id:4,href:"https://docs.paperd.ink/docs/hardware/hardware/",title:"5. Hardware",description:"Hardware details for paperd.ink",content:`\u003ch2 id="block-diagram"\u003eBlock Diagram\u003c/h2\u003e
-\u003cimg src="/images/paperd.ink_labels_2.png" width="600" height="400"\u003e
+\u003cimg src="/images/paperd.ink_rev4_labels.png" width="600" height="400"\u003e
 \u003col\u003e
 \u003cli\u003e\u003cstrong\u003eESP32-WROOM-32\u003c/strong\u003e module\u003c/li\u003e
 \u003cli\u003e\u003cstrong\u003eFCC/CE Certified\u003c/strong\u003e WiFi \u0026amp; Bluetooth for connectivity\u003c/li\u003e
@@ -81,7 +88,7 @@ var suggestions=document.getElementById("suggestions"),userinput=document.getEle
 \u003cli\u003e\u003cstrong\u003eExtension Header\u003c/strong\u003e for interfacing and expanding capabilities\u003c/li\u003e
 \u003c/ol\u003e
 \u003ch2 id="pin-map"\u003ePin Map\u003c/h2\u003e
-\u003cp\u003e\u003cimg src="/images/paperd.ink_pinout.png" alt="paperd.ink pinout"\u003e\u003c/p\u003e
+\u003cimg src="/images/paperd.ink_pinout.png" width="300"\u003e
 \u003col\u003e
 \u003cli\u003eUART, SPI, and I2C so you can add external sensors and modules. This makes the board extendable.\u003c/li\u003e
 \u003cli\u003e8 I/O for controlling any output and receiving any input.\u003c/li\u003e
@@ -90,66 +97,61 @@ var suggestions=document.getElementById("suggestions"),userinput=document.getEle
 \u003c/ol\u003e
 \u003ch5 id="i2c-pins"\u003eI2C pins\u003c/h5\u003e
 \u003cul\u003e
-\u003cli\u003eSDA: 16\u003c/li\u003e
-\u003cli\u003eSCL: 17\u003c/li\u003e
+\u003cli\u003eSDA: GPIO16\u003c/li\u003e
+\u003cli\u003eSCL: GPIO17\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch5 id="spi-pins"\u003eSPI pins\u003c/h5\u003e
 \u003cul\u003e
-\u003cli\u003eSCK: 18\u003c/li\u003e
-\u003cli\u003eMOSI: 23\u003c/li\u003e
-\u003cli\u003eMISO: 19\u003c/li\u003e
+\u003cli\u003eSCK: GPIO18\u003c/li\u003e
+\u003cli\u003eMOSI: GPIO23\u003c/li\u003e
+\u003cli\u003eMISO: GPIO19\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch5 id="sd-card-pins"\u003eSD card pins\u003c/h5\u003e
 \u003cul\u003e
-\u003cli\u003eSD CS: 21\u003c/li\u003e
-\u003cli\u003eSD enable: 5\u003c/li\u003e
+\u003cli\u003eSD CS: GPIO21\u003c/li\u003e
+\u003cli\u003eSD enable: GPIO5\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch5 id="e-paper-pins"\u003eE-paper pins\u003c/h5\u003e
 \u003cul\u003e
-\u003cli\u003eEPD DC: 15\u003c/li\u003e
-\u003cli\u003eEPD BUSY: 34\u003c/li\u003e
-\u003cli\u003eEPD RES: 13\u003c/li\u003e
-\u003cli\u003eEPD Enable: 12\u003c/li\u003e
+\u003cli\u003eEPD DC: GPIO15\u003c/li\u003e
+\u003cli\u003eEPD BUSY: GPIO34\u003c/li\u003e
+\u003cli\u003eEPD RES: GPIO13\u003c/li\u003e
+\u003cli\u003eEPD Enable: GPIO12\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch5 id="pcf8574-pins"\u003ePCF8574 pins\u003c/h5\u003e
 \u003cul\u003e
-\u003cli\u003ePCF INT: 35\u003c/li\u003e
+\u003cli\u003ePCF INT: GPIO35\u003c/li\u003e
 \u003cli\u003eSD Chip Detect (low = SD card present): P4\u003c/li\u003e
 \u003cli\u003eEXT GPIO1: P5\u003c/li\u003e
 \u003cli\u003eEXT GPIO2: P6\u003c/li\u003e
 \u003cli\u003eEXT GPIO3: P7\u003c/li\u003e
-\u003cli\u003ePCF I2C ADDR: 0x20\u003c/li\u003e
+\u003cli\u003ePCF I2C ADDR (Rev 3): 0x20\u003c/li\u003e
+\u003cli\u003ePCF I2C ADDR (Rev 4): 0x38\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch6 id="lipo"\u003eLiPo\u003c/h6\u003e
 \u003cul\u003e
-\u003cli\u003eCharging indicator (low = charging): 36\u003c/li\u003e
-\u003cli\u003eBattery Enable: 25\u003c/li\u003e
-\u003cli\u003eBattery Voltage: 34\u003c/li\u003e
+\u003cli\u003eCharging indicator (low = charging): GPIO36\u003c/li\u003e
+\u003cli\u003eBattery Enable: GPIO25\u003c/li\u003e
+\u003cli\u003eBattery Voltage: GPIO34\u003c/li\u003e
 \u003cli\u003eBattery Voltage ADC: ADC1_CHANNEL_6\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch5 id="buzzer"\u003eBuzzer\u003c/h5\u003e
 \u003cul\u003e
-\u003cli\u003ePiezo: 26\u003c/li\u003e
+\u003cli\u003ePiezo: GPIO26\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch5 id="buttons"\u003eButtons\u003c/h5\u003e
+\u003cp\u003eFrom top to bottom\u003c/p\u003e
 \u003cul\u003e
-\u003cli\u003eButton 1: 2\u003c/li\u003e
-\u003cli\u003eButton 2: 4\u003c/li\u003e
-\u003cli\u003eButton 3: 27\u003c/li\u003e
-\u003cli\u003eButton 4: 14\u003c/li\u003e
+\u003cli\u003eButton 1: GPIO2\u003c/li\u003e
+\u003cli\u003eButton 2: GPIO4\u003c/li\u003e
+\u003cli\u003eButton 3: GPIO27\u003c/li\u003e
+\u003cli\u003eButton 4: GPIO14\u003c/li\u003e
 \u003c/ul\u003e
 \u003ch2 id="interfacing-with-components"\u003eInterfacing with components\u003c/h2\u003e
 \u003cp\u003epaperd.ink is designed to be low power. To achieve this goal, the EPD, SD card and battery sense circuits are powered via a MOSFET.
 This MOSFET can be turned off when not required which helps reduce the power consumption. All enable pins are active low, meaning when
 the enable pin is low, the device is turned on. For example, to use the EPD, first set EPD enable (pin 12) to low and when done writing
 to the display set EPD enable to high to reduce the power consumption.\u003c/p\u003e
-\u003ch2 id="battery"\u003eBattery\u003c/h2\u003e
-\u003cp\u003eBatteries have shipping issues in terms of regulations and need special arrangements
-(which also increases the cost) so we recommend you buy a battery locally or you can keep the USB plugged in.
-The maximum battery size should be 62 x 35 x 5 mm.
-While selecting a battery for paperd.ink please note the polarity of the connection.
-A battery with in-built protection circuit is recommended.\u003c/p\u003e
-\u003cp\u003e\u003cimg src="/images/battery_connector.png" alt="paperd.ink battery"\u003e\u003c/p\u003e
 \u003ch2 id="design-files"\u003eDesign files\u003c/h2\u003e
 \u003cp\u003e\u003ca href="https://github.com/paperdink/paperd.ink_hw"\u003eGitHub →\u003c/a\u003e\u003c/p\u003e
 \u003ch2 id="dimensions"\u003eDimensions\u003c/h2\u003e
