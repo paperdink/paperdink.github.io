8153d8a4b13de4e102c90faef6d07f17f2563ee3
robertlangdonn <robertlangdonn@users.noreply.github.com>
2022-12-06 06:21:31 +0000

    deploy: 6e1c4e2a33cad28e83ec5853025ee4393c40e725

--- index.min.d3b3bdad436e5f5d2d5b8778821dbb10eba6d8c6befbd7e17bc8e88cde69c87d6c8afed63f181afe6e25f060ee462ead3a588999028b6cb1506f0b8f791c65c3.js
+++ index.min.d3b3bdad436e5f5d2d5b8778821dbb10eba6d8c6befbd7e17bc8e88cde69c87d6c8afed63f181afe6e25f060ee462ead3a588999028b6cb1506f0b8f791c65c3.js
@@ -52,17 +52,17 @@ var suggestions=document.getElementById("suggestions"),userinput=document.getEle
 \u003cli\u003eJSON Streaming Parser by Daniel Eichhorn\u003c/li\u003e
 \u003c/ul\u003e
 `},{id:2,href:"https://docs.paperd.ink/docs/software/examples/",title:"3. Examples",description:"Sample codes and use-cases to get started",content:`\u003ch1 id="paperdink-arduino-library-examples"\u003ePaperdink Arduino Library Examples\u003c/h1\u003e
-\u003cp\u003eFollow instructions \u003ca href="https://docs.paperd.ink/docs/software/getting-started/#paperdink-library"\u003ehere\u003c/a\u003e to get started with the Paperdink library.\u003c/p\u003e
+\u003cp\u003eTo get started with the Paperdink library follow the instructions \u003ca href="https://docs.paperd.ink/docs/software/getting-started/#paperdink-library"\u003ehere→\u003c/a\u003e .\u003c/p\u003e
 \u003ch2 id="crypto-tracker"\u003eCrypto Tracker\u003c/h2\u003e
-\u003cp\u003eTrack your cryptocurrency investments.\u003c/p\u003e
+\u003cp\u003eTrack your cryptocurrency investments to see how much you\u0026rsquo;re in the red ;p\u003c/p\u003e
 \u003cp\u003e\u003cstrong\u003e\u003ca href="https://github.com/paperdink/PaperdInk-Library/tree/main/examples/Crypto_Tracker"\u003ehttps://github.com/paperdink/PaperdInk-Library/tree/main/examples/Crypto_Tracker\u003c/a\u003e\u003c/strong\u003e\u003c/p\u003e
 \u003cimg src="https://github.com/paperdink/PaperdInk-Library/raw/main/examples/Crypto_Tracker/Crypto_Tracker.png" width="600" height="400"\u003e
 \u003ch2 id="youtube-stats"\u003eYoutube stats\u003c/h2\u003e
-\u003cp\u003eShow off your youtube views and subscribers count.\u003c/p\u003e
+\u003cp\u003eShow off your youtube views and subscribers count\u003c/p\u003e
 \u003cp\u003e\u003cstrong\u003e\u003ca href="https://github.com/paperdink/PaperdInk-Library/tree/main/examples/Youtube_Stats"\u003ehttps://github.com/paperdink/PaperdInk-Library/tree/main/examples/Youtube_Stats\u003c/a\u003e\u003c/strong\u003e\u003c/p\u003e
 \u003cimg src="https://github.com/paperdink/PaperdInk-Library/raw/main/examples/Youtube_Stats/Youtube_Stats.png" width="600" height="400"\u003e
 \u003ch2 id="wave"\u003eWave\u003c/h2\u003e
-\u003cp\u003eA clean dashboard with weather, date and day, up to 11 to-do items. Syncs to-do list with taiga.\u003c/p\u003e
+\u003cp\u003eA clean dashboard with weather, date and day, up to 11 to-do items. Syncs to-do list with Taiga.\u003c/p\u003e
 \u003cp\u003e\u003cstrong\u003e\u003ca href="https://github.com/paperdink/PaperdInk-Library/tree/main/examples/Wave"\u003ehttps://github.com/paperdink/PaperdInk-Library/tree/main/examples/Wave\u003c/a\u003e\u003c/strong\u003e\u003c/p\u003e
 \u003cimg src="https://github.com/paperdink/PaperdInk-Library/raw/main/examples/Wave/Wave.png" width="600" height="400"\u003e
 \u003ch1 id="other-examples"\u003eOther Examples\u003c/h1\u003e
