d66d3dcb6e49b5a356755dbb8a9c27fdc53875ac
robertlangdonn <robertlangdonn@users.noreply.github.com>
2022-12-06 06:32:38 +0000

    deploy: adce9b41b70256ee8c4118a00dd14f91a7cd186e

--- index.min.453cd880464e5d8d42c63da94e97975094d69c75443884f8b7022bc144480def2b9dbfe1fbb4b90ce8ccad6b37f1aae64639b530a0a90dcfb6b9332d63391555.js
+++ index.min.453cd880464e5d8d42c63da94e97975094d69c75443884f8b7022bc144480def2b9dbfe1fbb4b90ce8ccad6b37f1aae64639b530a0a90dcfb6b9332d63391555.js
@@ -171,9 +171,8 @@ var suggestions=document.getElementById("suggestions"),userinput=document.getEle
 \u003cp\u003ePaperd.ink is designed to be power efficient. Thus, the electronic paper display (EPD), SD card, and battery sense circuits are powered by a MOSFET that can be turned off when not in use. All enable pins are active low, meaning they must be set to low to turn on the circuitry.\u003c/p\u003e
 \u003cp\u003eFor example, to use the EPD, set EPD enable pin (GPIO 12) to low to provide power to the EPD and perform necessary display functions. Once display is updated set the EPD enable pin (GPIO 12) to high to reduce power consumption.\u003c/p\u003e
 \u003ch2 id="battery-specifications"\u003eBattery specifications\u003c/h2\u003e
-\u003cp\u003epaperd.ink classic and paperd.ink merlot comes with a 3.7V (nominal) 1900 mAh LiPo battery with a 4.2V charging voltage.
-paperd.ink devices have an inbuilt LiPo charger and the battery can be charged by connecting a type-C cable. During charging, the
-red LED lights up and once charging is completed the LED will switch off.\u003c/p\u003e
+\u003cp\u003epaperd.ink Classic and paperd.ink Merlot come equipped with a 3.7V (nominal) 1900 mAh LiPo battery, which offers extended battery life for your e-paper projects.\u003c/p\u003e
+\u003cp\u003epaperd.ink devices have an inbuilt LiPo charger so the battery can be charged using a 4.2V charging voltage. Simply connect a type-C cable to charge the battery, and the red LED will light up to indicate that charging is in progress. Once charging is complete, the LED will switch off.\u003c/p\u003e
 \u003ch2 id="design-files"\u003eDesign files\u003c/h2\u003e
 \u003cp\u003e\u003ca href="https://github.com/paperdink/paperd.ink_hw"\u003eGitHub →\u003c/a\u003e\u003c/p\u003e
 \u003ch2 id="dimensions"\u003eDimensions\u003c/h2\u003e
